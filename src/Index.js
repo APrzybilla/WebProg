e46de9a7beef57dd8b292ab86a4ea9e428725877
haushaelter <56068433+haushaelter@users.noreply.github.com>
2019-10-21 09:39:09 +0200

    Update Index.js

--- src/Index.js
+++ src/Index.js
@@ -1,7 +1,31 @@
 function suchen(){
     console.log("test");
-    /*Einfügen von neue Zeile an erster Stelle in der Tabelle*/
-    let neueTr = document.getElementById("Phasentabelle").insertRow(1);
+    /*Einfügen des Studenten*/
+    /*Einfügen von neuer Zeile an erster Stelle in der Tabelle */
+    let neueTr = document.getElementById("Tabellenhead").insertRow(1);
+
+    /*erzeugen der Tabellenspalten*/
+    let tdName = document.createElement("td");
+    let tdHS = document.createElement("td");
+    let tdS = document.createElement("td");
+    let tdJG = document.createElement("td");
+
+    /*befüllen der Spalten*/
+    tdName.innerHTML = "Platzhalter";
+    tdHS.innerHTML = "Platzhalter";
+    tdS.innerHTML = "pLatzhalter";
+    tdJG.innerHTML  = "Platzhalter";
+
+    /*hinzufügen der Spalten*/
+    neueTr.appendChild(tdName);
+    neueTr.appendChild(tdHS);
+    neueTr.appendChild(tdS);
+    neueTr.appendChild(tdJG);
+
+
+    /*Einfügen der Kalenderwochen*/
+    /*Einfügen von neuer Zeile an erster Stelle in der Tabelle*/
+    neueTr = document.getElementById("Tabellenbody").insertRow(1);
         
     for(let i = 1; i<53; i++){
         /*erzeugen der Tabellenspalten*/ 
