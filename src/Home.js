0c3cfa1ac18aaad033b6cc2b0e9ff764418b9052
haushaelter <56068433+haushaelter@users.noreply.github.com>
2019-10-22 16:17:25 +0200

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import stylesheet from "./SUebersicht.css";
+
+let _app="";
+
+class StartPage{
+    constructor(app){
+        this._app = app;
+        _app = this._app;
+    }
+
+    onShow(){
+
+    };
+
+    onLoad(){
+
+    }
+
+    onLeave(goon){
+        return true;
+    }
+}
+
+export default StartPage;
