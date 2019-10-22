bf44df81a285e39439aa675014821ba091bf193c
haushaelter <56068433+haushaelter@users.noreply.github.com>
2019-10-22 16:17:04 +0200

    Create Phasen.js

---
+++ src/Phasen.js
@@ -0,0 +1,24 @@
+import stylesheet from "./PUebersicht.css";
+
+let _app="";
+
+class Phasenuebersicht{
+    constructor(app){
+        this._app = app;
+        _app = this._app;
+    }
+
+    onShow(){
+
+    };
+
+    onLoad(){
+
+    }
+
+    onLeave(goon){
+        return true;
+    }
+}
+
+export default Phasenuebersicht;
