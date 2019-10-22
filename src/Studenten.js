1cc177179dfa691ea1006e97ebcd8c2f67cf9e26
haushaelter <56068433+haushaelter@users.noreply.github.com>
2019-10-22 16:17:01 +0200

    Create Studenten.js

---
+++ src/Studenten.js
@@ -0,0 +1,24 @@
+import stylesheet from "./Studenten.css";
+
+let _app="";
+
+class Studenten{
+    constructor(app){
+        this._app = app;
+        _app = this._app;
+    }
+
+    onShow(){
+
+    };
+
+    onLoad(){
+
+    }
+
+    onLeave(goon){
+        return true;
+    }
+}
+
+export default Studenten;
