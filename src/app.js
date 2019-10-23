2ccce1f9b2649c74faa4d9332aad7ea19307207c
haushaelter <56068433+haushaelter@users.noreply.github.com>
2019-10-23 08:49:58 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,40 +1,33 @@
 "use strict";
-//Main Class of Application
-
-import styelsheet from "./Index.css";
 
 import Navigo from "../node_modules/navigo/lib/navigo.js";
 
-import StartPage from "./Home.js";
+import Studenten from "./Studenten.js";
 import Phasenuebersicht from "./Phasen.js";
-import Studentenuebersicht from "./Studenten.js";
+import StartPage from "./Home.js";
 
-
-
-class App{
-    construtor(){
-        this._currentURL._title="Studentenuebersicht";
+class App {
+    constructor(){
+        this._title = "Studentenübersicht";
         this._currentView = null;
 
-        //Single Page Router initialisieren
         this._router = new Navigo();
-        this._currentURL="";
-        //Abfrage nach Abbruch durch den Nutzer
+        this._currentUrl = "";
         this._navAborted = false;
-        
+
         this._router.on({
-            "/Studentenuebersicht/Home": () => this.showPage("Home"),
-            "/Studentenuebersicht/Phasen": () => this.showPage("Phasen"),
-            "/Studentenuebersicht/Studenten": () => this.showPage("Studenten")
+            "/Home":        () => this.showPage("Home"),
+            "/Phasen":      () => this.showPage("Phasen"),
+            "/Studenten":   () => this.showPage("Studenten")
         });
 
         this._router.hooks({
-            after: (params) =>{
-                if(!this._navAborted){
+            after: (params) => {
+                if(!this._navAborted) {
                     this._currentUrl = this._router.lastRouteResolved().url;
                 } else {
                     this._router.pause(true);
-                    this._router.navigate(this.currentUrl);
+                    this._router.navigate(this._currentUrl);
                     this._router.pause(false);
                     this._navAborted = false;
                 }
@@ -42,33 +35,33 @@ class App{
         });
     }
 
-    start(){
-        console.log("App ist gestartet");
+    start() {
+        console.log("App started successfully :)");
         this._router.resolve();
     }
 
-    showPage(name){
-        console.log("showPage");
+    showPage(id){
         let view;
-        if(name=="Home"){
+        if(id =="Home"){
             view = new StartPage(this);
-        } else if(name=="Phasen"){
+        } else if (id == "Phasen"){
             view = new Phasenuebersicht(this);
-        } else if(name=="Studenten"){
+        } else if(id =="Studenten"){
             view = new Studenten(this);
         }
-        this._switchVisibleView(view);
+        if(view!==null){
+            this._switchVisibleView(view);
+        }
     }
-
-    _switchVisibleView(view){
-        console.log("switchView");
+    _switchVisibleView(view) {
         let newUrl = this._router.lastRouteResolved().url;
         console.log(newUrl);
-        let goon = () =>{
+        let goon = () => {
             this._router.navigate(newUrl + "?goon");
         }
-            
-        if(this._currentView && !this._currentView.onLeave(goon)){
+
+        if(this._currentView && !this._currentView.onLeave(goon)) {
+            console.log("Navigation aborted");
             this._navAborted = true;
             return false;
         }
@@ -82,32 +75,32 @@ class App{
     _switchVisibleContent(content) {
         // <header> und <main> des HTML-Grundgerüsts ermitteln
         let app = document.querySelector("#app");
-        let header = document.querySelector("#app > header");
+        //let header = document.querySelector("#app > header");
         let main = document.querySelector("#app > main");
-    
+
         // Zuvor angezeigte Inhalte entfernen
         // Bei der Topbar nur die untere Zeile, im Hauptbereich alles!
         app.className = "";
-        header.querySelectorAll(".bottom").forEach(e => e.parentNode.removeChild(e));
+        //header.querySelectorAll(".bottom").forEach(e => e.parentNode.removeChild(e));
         main.innerHTML = "";
-    
+
         // CSS-Klasse übernehmen, um die viewspezifischen CSS-Regeln zu aktivieren
         if (content && content.className) {
             app.className = content.className;
         }
-    
+
         // Neue Inhalte der Topbar einfügen
-        if (content && content.topbar) {
+        /*if (content && content.topbar) {
             content.topbar.forEach(element => {
                 element.classList.add("bottom");
                 header.appendChild(element);
             });
-        }
-    
+        }*/
+
         // Neue Inhalte des Hauptbereichs einfügen
         if (content && content.main) {
             content.main.forEach(element => {
-                main.appendChild(element);
+                app.appendChild(element);
             });
         }
         // Navigo an die Links in der View binden
@@ -118,29 +111,3 @@ class App{
 }
 
 export default App;
-
-        
-
-        /*Title, wenn man will
-        vor alles this.
-        currentView=null oder aktuelle Seite 
-        router = new Navigo
-        currentURL
-        navAborted = ob der Nutzer es abgebrochen hat
-        dann router.on erstellung
-        router.hooks = auf abrechen prüfen
-        dann current url neu bestimmen
-        wenn nicht zur currentUrl
-        start(): Router.resolve()
-        
-        onShow Methode
-        klonen von section
-        onLoad
-        onLeave: Formular, abfragen ob er verlassen will
-        
-        export default NameVonKlasse -> dass man es woanders importieren kann
-        
-        
-        _switchVisibleView: sorgt dafür, dass es angezeigt wird
-        URL holen
-        this._router.updatePageLinks()*/
