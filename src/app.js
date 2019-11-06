4d1fa631a2a7d62b26ebd1fd009ef78ce64d9bb7
haushaelter <56068433+haushaelter@users.noreply.github.com>
2019-11-06 17:57:47 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,9 +21,11 @@ class App {
         this._db.createDemoData();
 
         this._router.on({
+            "/":            () => this.showMainPage(),
             "/Home":        () => this.showMainPage(),
             "/Phasen":      () => this.showPhasenPage(),
-            "/Studenten":   () => this.showStudentenPage()
+            "/Studenten":   () => this.showStudentenPage(),
+            "/Studenten/*": () => this.showStudentenPage()
         });
 
         this._router.hooks({
